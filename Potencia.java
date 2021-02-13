9a4cd55ad068aeb35b94d6897e5238ee9ad549f5
Eva Quintana Ramírez <evaquintanaramirez@MacBook-Pro-de-Eva.local>
2021-02-13 21:57:24 +0100

    potencia

---
+++ Potencia.java
@@ -0,0 +1,88 @@
+package calculadora;
+/**
+ * Esta clase implementará todos los métodos que están relacionados con la multiplicación de la calculadora.
+ * @author Eva
+ * @since 23/01/2021
+ * @version 1.0
+ */
+
+public class Potencia {
+	/**
+	 * Producto de dos numeros reales, tendra dos parametros de entrada y uno de salida que sera la solucion
+	 * 
+	 * *@param real x
+	 * *@param real y
+	 * *@return null 
+	 * **
+	 * 
+	 */
+	
+	public int potencia1 (int x, int y) {
+		
+		
+		return 0; 
+
+	}
+
+
+/**
+ * 
+Producto de dos numeros enteros,tendra dos parametros de entrada y uno de salida que sera la solucion
+ * 
+ * *@param x
+ * 			Valor numerico 1
+ * *@param y
+ * 			Valor numerico 2
+ * *@return null
+ * **
+ * 
+ 
+ */
+	
+	public int potencia2 (int x, int y) {
+	
+
+	return 0;
+}
+
+
+/**
+ * 
+ * Producto de tres numeros reales,tendra tres parametros de entrada y uno de salida que sera la solucion
+ * 
+ * *@param x
+ * *@param y
+ * *@param z
+ * *@return null
+ * **
+ * 
+ */
+
+	public int potencia3 (int x, int y, int z) {
+	
+	return 0; 
+}
+
+
+/**
+ * Potencia, tendra dos parametros de entrada (base y exponente) y uno de salida que sera la solucion
+ * 
+ * *@param m
+ * 			Valor numerico base
+ * *@param n
+ * 			Valor numerico exponente
+ * *@return null
+ * **
+ * 
+ /**
+  
+  */
+ 
+	public int potencia4 (int m, int n) {
+
+	return 0;
+
+	}
+}
+
+
