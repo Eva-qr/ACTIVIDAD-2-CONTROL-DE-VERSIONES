3a897cbd19130406e8b29733086b0c90918d1c7d
Gemma MG <gemmi27@gmail.com>
2021-02-13 21:01:28 +0100

    añado resto de datos

--- Resta.java
+++ Resta.java
@@ -7,3 +7,57 @@ package calculadora;
  *  @version 1.0
  */
 
+public class Resta {
+	/**
+	 * Este metodo resta de dos números reales, tendrá 2 parámetros de entrada y uno de salida que será la solución.
+	 * 
+	 * @param oper1 es el primer numero que se va a restar
+	 * @param oper2 es el segundo numero que se va a restar
+	 * 
+	 * @return la resta de los dos numeros. 
+	 * 
+	 * @throws excepcion: los numeros no pueden ser negativos.
+	 */
+
+public int Resta1(int oper1, int oper2) {
+	return oper1 - oper2;
+}
+    /**
+     * Este metodo esta de dos numeros enteros, tendra 2 parametros de entrada y uno de salida que sera la solucion.
+     * 
+	 * @param oper1 es el primer numero a restar
+	 * @param oper2 es el segundo numero a restar
+     * 
+     * @return la resta de ambos numeros 
+     * 
+
+     */
+
+public int Resta2(int oper1, int oper2) {
+	return oper1 - oper2;
+}
+     /**
+      * Resta de tres números reales, tendrá 3 parámetros de entrada y uno de salida que será la solución.
+      * 
+	 * @param oper1 es el primer numero que se va a restar
+	 * @param oper2 es el segundo numero que se va a restar
+      * @param oper3 es el tercer numero que se va a restar
+      * 
+      * @return la resta de los tres numeros.
+      * 
+      */
+public int Resta3(int oper1, int oper2, int oper3) {
+	return oper1 - oper2 - oper3;
+}
+     /**
+      * Resta con valor acumulado, tendrá un parámetro de entrada y la clase deberá de guardar el valor acumulado.
+      * 
+	 * @param oper1 es el primer numero a resta
+      * 
+      * @return devuelve el valor acumulado de la resta de los numeros ya restados anteriormente.
+      * 
+      */
+
+public void Resta4() {
+	return;
+}
