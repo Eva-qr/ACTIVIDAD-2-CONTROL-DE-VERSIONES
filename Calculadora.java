bf12a71d24561c20d9bd094130cebfb087536eb2
RodrigoSendinoSanz <78493406+RodrigoSendinoSanz@users.noreply.github.com>
2021-02-14 19:23:07 +0100

    Calculadora

---
+++ Calculadora.java
@@ -0,0 +1,24 @@
+/*
+ * Queremos documentar una serie de clases que implementen una calculadora.
+ * Para ello hemos predefinido una serie de clases y métodos que el equipo tendrá que documentar.
+ * Nótese que NO hay que implementar el código, solo la declaración de los métodos y las clases y su documentación.
+ * Esta actividad está pensada para que cada miembro del equipo documente una clase.
+ */
+package calculadora;
+/**
+ * En este programa no se pueden insertar numeros negativos ya que no funcionaria.
+ * @author Ignacio, Rodrigo, Eva, Gemma.
+ * @since 24/01/2021
+ * @version 1.0
+ */
+public class Calculadora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Suma s = new Suma();
+		Cociente c = new Cociente();
+		Potencia p = new Potencia();
+		Resta r = new Resta();
+	}
+
+}
