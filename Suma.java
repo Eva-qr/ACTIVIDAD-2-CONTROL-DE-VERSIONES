e2f171189e4a60e68a9210243c65a0f60bf526d3
RodrigoSendinoSanz <78493406+RodrigoSendinoSanz@users.noreply.github.com>
2021-02-14 20:55:11 +0100

    Update Suma.java

--- Suma.java
+++ Suma.java
@@ -1,6 +1,8 @@
+package suma;
+
 	/**
-	 * Esta clase implementará todos los métodos que están relacionados con la sumas de la calculadora.
-	 * Los operandos no pueden ser números negativos
+	 * Esta clase implementara todos los metodos que esten relacionados con la sumas de la calculadora.
+	 * Los operandos no pueden ser numeros negativos
 	 * 
 	 * @author Rodri
 	 *
@@ -9,23 +11,24 @@
 public class Suma {
 	
 		/**
-		 * Suma de dos números reales de los parámetros operando1 y operando2, devolviendo su suma.
+		 * Suma de dos numeros reales de los parametros operando1 y operando2, devolviendo su suma.
 		 * 
-		 * [Los operandos no pueden ser números negativos]
+		 * [Los operandos no pueden ser numeros negativos]
 		 * 
 		 * @param operando1 Primer operando que se sumara a operando2
 		 * @param operando2 Segundo opernado que se sumara a el opernado1
 		 * @return
 		 */
 	
-	public int sumar1(int operando1,int operando2){
-		return 0;
+	public double sumar1(double operando1,double operando2){
+            
+		return (operando1+operando2);
 	}
 	
 		/**
-		 * Suma de dos números enteros de los parámetros operando1 y operando2, devolviendo su suma.
+		 * Suma de dos numeros enteros de los parametros operando1 y operando2, devolviendo su suma.
 		 * 
-		 * [Los operandos no pueden ser números negativos]
+		 * [Los operandos no pueden ser numeros negativos]
 		 * 
 		 * @param operando1 Primer operando que se sumara a operando2
 		 * @param operando2 Segundo opernado que se sumara a el opernado1
@@ -33,13 +36,13 @@ public class Suma {
 		 */
 	
 	public int sumar2(int operando1,int operando2){
-		return 0;
+		return (operando1+operando2);
 	}
 	
 		/**
-		 * Suma de tres números reales, de los operandos 1,2 y 3 dados en los parámetros de entrada, devolviendo la suma de los mismos
+		 * Suma de tres numeros reales, de los operandos 1,2 y 3 dados en los parametros de entrada, devolviendo la suma de los mismos
 		 * 
-		 * [Los operandos no pueden ser números negativos]
+		 * [Los operandos no pueden ser numeros negativos]
 		 * 
 		 * @param operando1 Primer operando que se sumara a operando2
 		 * @param operando2 Segundo opernado que se sumara a el opernado1
@@ -47,19 +50,22 @@ public class Suma {
 		 * @return
 		 */
 	
-	public int sumar3(int operando1,int operando2,int operando3){
-		return 0;
+	public double sumar3(double operando1,double operando2,double operando3){
+		return (operando1+operando2+operando3);
 	}
 	
 		/**
 		 * Suma el valor acumulado, dado en el parametro de entrada y guardarndo el valor acumulado.
 		 * 
-		 * [Los operandos no pueden ser números negativos]
+		 * [Los operandos no pueden ser numeros negativos]
 		 * 
 		 * @param operando1 El operando colocado se ir sumando a los operndos colocados anteriormente en este metodo
-		 * @return
+		 * @return Resultado de la suma
 		 */
 	
-	public void sumar4(){
-	}
+	public int sumar4(int operando1){
+            return 0;
+            
+
+    }
 }
